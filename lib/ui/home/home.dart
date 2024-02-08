4bf88111b04cd32d671b9d3d73d182ac2865930e
Gurly08 <bagas.dj89@gmail.com>
2024-02-08 08:49:53 +0700

    update

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,69 +1,143 @@
 import 'package:flutter/material.dart';
-import 'package:reproed/ui/home/widget/boxpoint.dart';
-import 'package:reproed/ui/home/widget/boxprofile.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Home extends StatefulWidget{
+  const Home ({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<Home> createState(){
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey[100],
-        body: ListView(
-          children: [
-            Stack(
-              children: [
-                //post banner banground
-                // ignore: sized_box_for_whitespace
-                Container(
-                  height: 300,
-                  width: 400,
-                  child: Image.asset('assets/images/cptframe.png',
-                      fit: BoxFit.cover, repeat: ImageRepeat.noRepeat),
-                ),
-
-                //profile konten
-                // ignore: avoid_unnecessary_containers
-                Container(
-                  child: Column(
-                    children: [
-                      //emohty gap
-                      const SizedBox(height: 64),
-
-                      // ignore: avoid_unnecessary_containers
-                      Container(
-                        child: const Center(
-                          child: Row(
-                            children: [
-                              Text('Hi! Bagas'),
-                              CircleAvatar(
-                                foregroundImage: AssetImage('assets/images/ppaku.png'),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-
-                      //card user
-                      // ignore: avoid_unnecessary_containers
-                      Container(
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            BoxProfile(),
-                            BoxPoint(),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            )
+        appBar: AppBar(
+          backgroundColor: Colors.grey[100],
+          title: Text(
+            'home',
+            style: GoogleFonts.poppins(
+              textStyle: TextStyle(
+                fontSize: 15,
+                color: Colors.blue[900],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed:() {}, 
+              icon: Icon(
+                Icons.notifications,
+                color: Colors.purple[900],
+              ),
+            ),
           ],
         ),
-      ),
-    );
+
+        body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Hi Jenifer",
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        color: Colors.blue[900],
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+            
+                  Text(
+                    "Selamat Pagi",
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        color: Color.fromARGB(221, 153, 153, 153),
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+            
+                  const SizedBox(height: 20),
+            
+                  Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 27, 18, 199),
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                             Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Total Poin",
+                                    style: GoogleFonts.poppins(
+                                      textStyle: TextStyle(
+                                        color: Colors.blue[800],
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    "Tukarkan poin ini untuk membuka kunci jawaban",
+                                    style: GoogleFonts.poppins(
+                                      textStyle: TextStyle(
+                                        color: Colors.grey[400],
+                                        fontSize: 5,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    "250",
+                                    style: GoogleFonts.poppins(
+                                      textStyle: TextStyle(
+                                        color: Colors.green[600],
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Image.asset(
+                            'assets/images/belajar.png',
+                            width: 150,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+            
+            
+                ],
+              ),
+            )
+          ),
+        ),
+      );
   }
 }
